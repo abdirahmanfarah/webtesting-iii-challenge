1ec879e069923c02b2e30585fd9f724cd45cc6d8
abdirahmanfarah <abdirahman.z.farah@gmail.com>
2019-12-16 16:38:21 -0800

    testing

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,15 @@
 // Test away!
+
+import React from 'react';
+import { render } from "@testing-library/react";
+
+import Display from "./Display";
+
+
+test('Is Locked being displayed', () => {
+  const { getByTestId, rerender } = render(<Display />)
+  getByTestId('locked');
+
+    rerender(<Display />);
+    getByTestId('closed');
+} )
